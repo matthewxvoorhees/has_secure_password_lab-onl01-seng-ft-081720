65a225a4efb86243b8a66ae4326563647cc9eaf2
matthewxvoorhees <matthew.crnich@gmail.com>
2020-10-30 11:18:10 -0600

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+        @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to 'user#show'
+        else
+            redirect_to '/login'
+        end
+    end
+
+    def destroy
+
+    end
+
+end
